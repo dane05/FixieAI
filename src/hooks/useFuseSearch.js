b66f5fa8cdb304429a42ca71d69b5e08c2f18f4e
dane05 <77316483+dane05@users.noreply.github.com>
2025-05-30 07:27:50 +0800

    Update useFuseSearch.js

--- src/hooks/useFuseSearch.js
+++ src/hooks/useFuseSearch.js
@@ -1,1 +1,14 @@
-export default function useFuseSearch() { return () => []; }
+import Fuse from "fuse.js";
+import { useMemo } from "react";
+
+const useFuseSearch = (data, keys = ["text"]) => {
+  const fuse = useMemo(() => new Fuse(data, {
+    includeScore: true,
+    threshold: 0.4,
+    keys
+  }), [data]);
+
+  return (query) => fuse.search(query).map(result => result.item);
+};
+
+export default useFuseSearch;
