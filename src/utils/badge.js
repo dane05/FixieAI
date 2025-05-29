8f571d2426e2b722856ec4e3c0132503fe3b6d83
dane05 <77316483+dane05@users.noreply.github.com>
2025-05-30 07:29:02 +0800

    Update badge.js

--- src/utils/badge.js
+++ src/utils/badge.js
@@ -1,1 +1,13 @@
-export const badgeLevels = []; export const getBadge = () => 'Newbie';
+export const badgeLevels = [
+  { title: "Newbie", points: 0 },
+  { title: "Junior Fixer", points: 15 },
+  { title: "Expert Fixer", points: 30 },
+  { title: "Master Fixer", points: 50 },
+];
+
+export const getBadge = (points) => {
+  for (let i = badgeLevels.length - 1; i >= 0; i--) {
+    if (points >= badgeLevels[i].points) return badgeLevels[i].title;
+  }
+  return "Newbie";
+};
