52c764e00cbb24d14d07d0f6e5e29f47b629ed97
dane05 <77316483+dane05@users.noreply.github.com>
2025-05-30 07:25:07 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-const firebaseConfig = {}; const app = initializeApp(firebaseConfig); export const db = getFirestore(app);
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCO2vyNj3y_g3V4u0SuwWGfNWm6MuzFUs8",
+  authDomain: "fixieaibot.firebaseapp.com",
+  projectId: "fixieaibot",
+  storageBucket: "fixieaibot.firebasestorage.app",
+  messagingSenderId: "53852730024",
+  appId: "1:53852730024:web:431b57703f48d87be0089d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
