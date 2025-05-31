b240672b63c1b8f5bf448d16403d6c446e1f8e0d
dane05 <77316483+dane05@users.noreply.github.com>
2025-05-31 08:30:29 +0800

    Update ChatMessages.jsx

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { marked } from 'marked';
+import ReactMarkdown from 'react-markdown';
 
-function renderMessage(content) {
-  const html = marked(content);
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-}
-
-const ChatMessages = ({ messages }) => {
-  return (
-    <div className="chat-messages">
-      {messages.map((msg, index) => (
-        <div
-          key={index}
-          className={`message ${msg.sender === "user" ? "user-message" : "bot-message"}`}
-        >
-          {msg.typing ? <em>{msg.text}</em> : msg.text}
-        </div>
-      ))}
-    </div>
-  );
-};
+const ChatMessages = ({ messages }) => (
+  <div className="flex flex-col space-y-4 p-4 overflow-y-auto">
+    {messages.map((msg, idx) => (
+      <div
+        key={idx}
+        className={`max-w-xl px-4 py-3 rounded-lg shadow text-sm whitespace-pre-wrap ${
+          msg.sender === 'user'
+            ? 'bg-blue-600 text-white self-end'
+            : 'bg-gray-100 text-gray-800 self-start'
+        }`}
+      >
+        <ReactMarkdown>{msg.text}</ReactMarkdown>
+      </div>
+    ))}
+  </div>
+);
 
 export default ChatMessages;
